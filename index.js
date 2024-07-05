6e089e0abbec1ba1a3f265dc994b2bf69556308e
shantal rohbi <shantal.rohbi@student.moringaschool.com>
2024-07-05 10:03:30 +0300

    complete control flow lab assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet( distance){
   // Write your code here!
+  if (distance <= 400){
+    return "This is on me";
+  }else if (distance >= 400 && distance < 2000){
+    return " That will be  20 bucks.";
+  }else if ( distance > 2000 && distance < 2499){
+    return " I will gladly take your  30 bucks";
+  }else if ( distance > 2500){
+    return " No can do"
+  }
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity( city){
   // Write your code here!
+    if (city ===  NYC){
+      return  " Ok sounds good"
+    } else if (city ==! NYC ){
+      return " No go "
+    } 
+
+  }
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+switch (tip){
+  case 'generous':
+  return ' Thank you so much .';
+  break;
+  case ' not as generous':
+    return ' Thnakyou'
+    break;
+    default:
+      return 'bye'
+    
+
+
+
+
+}
+
 }
